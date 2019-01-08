c8c80e026ad8b8b7ea6c2c60c36a2a9be45381e6
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-08 21:48:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,9 @@ def reverse_each_word
   puts "Hello there, and how are you?".reverse
   end
 end
+
+def reverse_each_word
+  array.collect do |name|
+ "Hello there, and how are you?".reverse
+  end
+end
